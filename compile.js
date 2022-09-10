69ab61d5cac3673f6a91366099ed8d3171fcae34
bhaktibuana <bhaktibuana19@gmail.com>
2022-09-10 16:23:08 +0700

    create compile for the contract with web3

---
+++ compile.js
@@ -0,0 +1,29 @@
+const solc = require("solc");
+const path = require("path");
+const fs = require("fs");
+
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "Lottery.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+module.exports = {
+  abi: output.contracts["Lottery.sol"]["Lottery"].abi,
+  bytecode: output.contracts["Lottery.sol"]["Lottery"].evm.bytecode.object,
+};
